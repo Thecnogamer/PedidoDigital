6849f25ea3e513f3243991223eca3c0974fbfaa5
Pedro Borges <world.pedro@live.com>
2021-10-06 13:41:48 -0300

    modal produto

--- listaproduto.php
+++ listaproduto.php
@@ -1,82 +1,141 @@
 <!doctype html>
 <html>
-   <head>
-    <meta charset="UTF-8">
-    <title>Pedido Digital - Inserir Produto</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-   </head>
-   <body background-color="#fffff4">
 
-   <?php include "navbar.php"; ?>
+<head>
+  <meta charset="UTF-8">
+  <title>Pedido Digital - Inserir Produto</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
 
-        <div class="container">
-            <br>
-            <br>
-            <div class="row row-cols-3">
-    
-    
- 
-        <?php 
-            
-            
+<body background-color="#fffff4">
 
-            include "conn.php";
+  <?php include "navbar.php"; ?>
 
-            $sql="select * from produto";
+  <div class="container">
+    <br>
+    <br>
+    <div class="row row-cols-3">
 
-            $resul=mysqli_query($conn,$sql);
 
-            while($dados=mysqli_fetch_assoc($resul)){
 
-                
-                $id = $dados["id"];
-                $n = $dados["nome"];
-                $p = $dados["preco"];
-                $i = $dados["foto"];
-                $d = $dados["descricao"];
-                $a = $dados["adendum"];
-                $pastaArquivos = "imagens/produtos/";
+      <?php
 
-                echo "
+
+
+      include "conn.php";
+
+      $sql = "select * from produto";
+
+      $resul = mysqli_query($conn, $sql);
+
+      while ($dados = mysqli_fetch_assoc($resul)) {
+
+
+        $id = $dados["id"];
+        $n = $dados["nome"];
+        $p = $dados["preco"];
+        $i = $dados["foto"];
+        $d = $dados["descricao"];
+        $a = $dados["adendum"];
+        $t = $dados["tipo"];
+        $pastaArquivos = "imagens/produtos/";
+
+        echo "
                 <div class='col'>
-                    <div class='card' style='width: 18rem; cursor:pointer;' data-bs-toggle='modal' data-bs-target='#query'>
-                        <img src='$pastaArquivos$i' class='card-img-top' alt='$n' style='height: 150px; object-fit: cover'>
+                    <div class='card' onclick='modalquery(this)' style='width: 18rem; cursor:pointer;' data-bs-toggle='modal' data-bs-target='#query'>
+                        <img id='fotoquery' src='$pastaArquivos$i' class='card-img-top' alt='$n' style='height: 150px; object-fit: cover'>
                         <div class='card-body'>
-                            <h5 class='card-title'>$n</h5>
-                            <p class='card-text'>R$: $p</p>
-                            <p style='display:none;'>$id</p>
-                            <p style='display:none;'>$d</p>
-                            <p style='display:none;'>$a</p>
+                            <h5 id='nomequery' class='card-title titulo'>$n</h5>
+                            <p id='precoquery' class='card-text'>R$: $p</p>
+                            <p id='idquery' style='display:none;'>$id</p>
+                            <p id='descquery' style='display:none;'>$d</p>
+                            <p id='addquery' style='display:none;'>$a</p>
+                            <p id='tipoquery' style='display:none;'>$t</p>
                         </div>
                     </div>
                 </div>
                 ";
-            }
-        
-        
-        
-        
-        ?>
+      }
 
-<div class="modal fade" id="query" tabindex="-1" aria-labelledby="queryLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="queryLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+
+
+      ?>
+
+      <div class="modal fade" id="query" tabindex="-1" aria-labelledby="queryLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title titulo" id="queryLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <div class="col">
+                  <img src="" id="queryfoto" style="height: 160px; vertical-align: middle;">
+                </div>
+                <div class="col">
+                  <p class="titulo" id="querypreco"></p>
+                  <p class="titulo" id="querytipo"></p>
+                  <h6 class="titulo">Ingredientes:</h6>
+                  <p id="querydesc"></p>
+                  <h6 class="titulo">Personalização:</h6>
+                  <p id="queryadd"></p>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-      </div>
+
+
+      <script>
+        function modalquery(modal) {
+
+          var nome = modal.querySelector("#nomequery").textContent;
+          var foto = modal.querySelector("#fotoquery").src;
+          var preco = modal.querySelector("#precoquery").textContent;
+          var desc = modal.querySelector("#descquery").textContent;
+          var adendum = modal.querySelector("#addquery").textContent;
+          var tipo = modal.querySelector("#tipoquery").textContent;
+
+          document.querySelector("#queryLabel").innerText = nome;
+          document.querySelector("#queryfoto").src = foto;
+          document.querySelector("#querypreco").innerText = preco;
+          document.querySelector("#querydesc").innerText = desc;
+          document.querySelector("#queryadd").innerText = adendum;
+
+          switch (tipo) {
+
+            case "1":
+              document.querySelector("#querytipo").innerText = "Prato Feito";
+              break;
+
+            case "2":
+              document.querySelector("#querytipo").innerText = "Porção";
+              break;
+
+            case "3":
+              document.querySelector("#querytipo").innerText = "Bebida";
+              break;
+
+            case "4":
+              document.querySelector("#querytipo").innerText = "Sobremesa";
+              break;
+
+            default:
+              document.querySelector("#querytipo").innerText = "wtf?";
+              break;
+
+          }
+        }
+      </script>
+
     </div>
   </div>
-</div>
+</body>
 
-
-            </div>
-        </div>
-   </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,44 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="UTF-8">
-    <title>Index</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link href="CSS/Style.css" rel="stylesheet" type="text/css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta charset="UTF-8">
+  <title>Index</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <link href="CSS/Style.css" rel="stylesheet" type="text/css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
+
 <body>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #fffde4;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">
-      <img src="imagens/logomin.png" alt="logo" width="30" height="30" class="d-inline-block align-text-top">
-      Pedido Digital
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Produtos
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="listaproduto.php">Tudo</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Pratos Feitos</a></li>
-            <li><a class="dropdown-item" href="#">Porções</a></li>
-            <li><a class="dropdown-item" href="#">Bebidas</a></li>
-            <li><a class="dropdown-item" href="#">Sobremesas</a></li>
-          </ul>
-        </li>
-        <?php
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #fffde4;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="imagens/logomin.png" alt="logo" width="30" height="30" class="d-inline-block align-text-top">
+        Pedido Digital
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Produtos
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="listaproduto.php">Tudo</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Pratos Feitos</a></li>
+              <li><a class="dropdown-item" href="#">Porções</a></li>
+              <li><a class="dropdown-item" href="#">Bebidas</a></li>
+              <li><a class="dropdown-item" href="#">Sobremesas</a></li>
+            </ul>
+          </li>
+          <?php
           session_start();
-            if ($_SESSION["adm"] == "ativar") {
-              echo "<li class='nav-item dropdown'>
+          if ($_SESSION["adm"] == "ativar") {
+            echo "<li class='nav-item dropdown'>
               <a class='nav-link dropdown-toggle' href='#' id='navbarDropdownMenuLink' data-bs-toggle='dropdown' aria-expanded='false'>
               Modo ADM
               </a>
@@ -46,22 +50,22 @@
                 <a class='dropdown-item' href='listaprodutoadm.php'>Lista ADM</a>
                 <a class='dropdown-item' href='frminserirproduto.php'>Inserir Produto</a>
               </div>
-            </li>"; 
-            }
+            </li>";
+          }
           ?>
         </ul>
         <?php
         if ($_SESSION["adm"] == "ativar") {
-          echo"<a class='btn btn-danger' href='senhaadmsair.php' role='button'>Sair do modo Adm</a>";
-        }
-        else {
-          echo"<a class='btn btn-success' href='senhaadmentrar.php' role='button'>Entrar no modo Adm</a>";
+          echo "<a class='btn btn-danger' href='senhaadmsair.php' role='button'>Sair do modo Adm</a>";
+        } else {
+          echo "<a class='btn btn-success' href='senhaadmentrar.php' role='button'>Entrar no modo Adm</a>";
         }
         ?>
+      </div>
     </div>
-  </div>
-</nav>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+  </nav>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
 </body>
+
 </html>
